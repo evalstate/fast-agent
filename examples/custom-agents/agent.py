f66f20e20e3b45963d4de2400d0befde12525760
evalstate <1936278+evalstate@users.noreply.github.com>
2025-06-12 08:40:29 +0100

    lint

--- examples/custom-agents/agent.py
+++ examples/custom-agents/agent.py
@@ -1,14 +1,17 @@
 import asyncio
-from mcp_agent.core.fastagent import FastAgent
+
 from mcp_agent.agents.base_agent import BaseAgent
+from mcp_agent.core.fastagent import FastAgent
 
 # Create the application
 fast = FastAgent("fast-agent example")
+
 
 class MyAgent(BaseAgent):
     async def initialize(self):
         await super().initialize()
         print("it's a-me!...Mario!")
+
 
 # Define the agent
 @fast.custom(MyAgent, instruction="You are a helpful AI Agent")
