a4aaa03ea740b0e4528a3534915c8d93cf6378e0
ChrisW-priv <chris.watras@gmail.com>
2025-10-01 19:28:04 +0200

    feat: add McpAgent example

--- examples/new-api/simple_mcp.py
+++ examples/new-api/simple_mcp.py
@@ -1,26 +1,46 @@
+"""
+Demonstration of changing MCP settings dynamically.
+
+Technically applicable to any setting, but MCP I found to be the most difficult due to MCP Aggregation at the startup.
+"""
+
 import asyncio
 
-from fast_agent.core import Core
+from fast_agent.agents.agent_types import AgentConfig
+from fast_agent.agents.mcp_agent import McpAgent
+from fast_agent.core.core_app import Core
+from fast_agent.llm.model_factory import ModelFactory
 
 
 async def main():
     core: Core = Core()
     await core.initialize()
 
-    # # Create agent configuration
-    # config = AgentConfig(name="weather_bot", model="haiku")
+    # Create agent configuration
+    config = AgentConfig(
+        name="dynamic_bot",
+        model="gpt-4o-mini",
+        servers=[  # Do not forget to add the servers here
+            "fetch",
+        ],
+    )
 
-    # tool_agent = McpAgent(
-    #     config,
-    #     context=core.context,
-    # )
+    agent = McpAgent(
+        config,
+        connection_persistence=True,
+        context=core.context,
+    )
 
-    # # Attach the LLM
-    # await tool_agent.attach_llm(ModelFactory.create_factory("haiku"))
+    # Attach the LLM
+    await agent.attach_llm(ModelFactory.create_factory("gpt-4o-mini"))
+    await agent.initialize()  # MCP Agents need to be initialized before use
 
-    # # Test the agent
-    # await tool_agent.send("What's the weather like in San Francisco and what's the temperature?")
-    # await core.cleanup()
+    # Test the agent
+    result = await agent.send(
+        "Tell me about fast-agent framework. Find info at https://fast-agent.ai/ and summarize it."
+    )
+    print(result)
+    await core.cleanup()
 
 
 if __name__ == "__main__":
