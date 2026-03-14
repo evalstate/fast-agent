67c6ae68373266000f2ca66944998b779fc093dd
evalstate <1936278+evalstate@users.noreply.github.com>
2026-03-14 12:50:33 +0000

    rename prompt session modules to input

--- src/fast_agent/ui/enhanced_prompt.py
+++ src/fast_agent/ui/enhanced_prompt.py
@@ -14,21 +14,21 @@ from fast_agent.ui.prompt.alert_flags import (
 )
 from fast_agent.ui.prompt.completer import AgentCompleter
 from fast_agent.ui.prompt.editor import get_text_from_editor
-from fast_agent.ui.prompt.keybindings import AgentKeyBindings, create_keybindings
-from fast_agent.ui.prompt.parser import parse_special_input
-from fast_agent.ui.prompt.session import (
+from fast_agent.ui.prompt.input import (
     ShellPrefixLexer,
     _display_agent_info_helper,
     get_argument_input,
     get_selection_input,
     show_mcp_status,
 )
-from fast_agent.ui.prompt.session import (
+from fast_agent.ui.prompt.input import (
     get_enhanced_input as _get_enhanced_input,
 )
-from fast_agent.ui.prompt.session import (
+from fast_agent.ui.prompt.input import (
     handle_special_commands as _handle_special_commands,
 )
+from fast_agent.ui.prompt.keybindings import AgentKeyBindings, create_keybindings
+from fast_agent.ui.prompt.parser import parse_special_input
 from fast_agent.ui.prompt.toolbar import (
     _can_fit_shell_path_and_version,
     _fit_shell_identity_for_toolbar,
@@ -59,17 +59,17 @@ _startup_notices: list[object] = []
 
 
 def _sync_to_session() -> None:
-    from fast_agent.ui.prompt import session as _session
+    from fast_agent.ui.prompt import input as _input
 
-    _session.agent_histories = agent_histories
-    _session.available_agents = available_agents
-    _session.in_multiline_mode = in_multiline_mode
-    _session._last_copyable_output = _last_copyable_output
-    _session._copy_notice = _copy_notice
-    _session._copy_notice_until = _copy_notice_until
-    _session.help_message_shown = help_message_shown
-    _session._agent_info_shown = _agent_info_shown
-    _session._startup_notices = _startup_notices
+    _input.agent_histories = agent_histories
+    _input.available_agents = available_agents
+    _input.in_multiline_mode = in_multiline_mode
+    _input._last_copyable_output = _last_copyable_output
+    _input._copy_notice = _copy_notice
+    _input._copy_notice_until = _copy_notice_until
+    _input.help_message_shown = help_message_shown
+    _input._agent_info_shown = _agent_info_shown
+    _input._startup_notices = _startup_notices
 
 
 def _sync_from_session() -> None:
@@ -77,17 +77,17 @@ def _sync_from_session() -> None:
     global _last_copyable_output, _copy_notice, _copy_notice_until
     global help_message_shown, _agent_info_shown, _startup_notices
 
-    from fast_agent.ui.prompt import session as _session
+    from fast_agent.ui.prompt import input as _input
 
-    agent_histories = _session.agent_histories
-    available_agents = _session.available_agents
-    in_multiline_mode = _session.in_multiline_mode
-    _last_copyable_output = _session._last_copyable_output
-    _copy_notice = _session._copy_notice
-    _copy_notice_until = _session._copy_notice_until
-    help_message_shown = _session.help_message_shown
-    _agent_info_shown = _session._agent_info_shown
-    _startup_notices = _session._startup_notices
+    agent_histories = _input.agent_histories
+    available_agents = _input.available_agents
+    in_multiline_mode = _input.in_multiline_mode
+    _last_copyable_output = _input._last_copyable_output
+    _copy_notice = _input._copy_notice
+    _copy_notice_until = _input._copy_notice_until
+    help_message_shown = _input.help_message_shown
+    _agent_info_shown = _input._agent_info_shown
+    _startup_notices = _input._startup_notices
 
 
 def set_last_copyable_output(output: str) -> None:
@@ -97,7 +97,7 @@ def set_last_copyable_output(output: str) -> None:
 
 
 def queue_startup_notice(notice: object) -> None:
-    from fast_agent.ui.prompt.session import queue_startup_notice as _queue_startup_notice
+    from fast_agent.ui.prompt.input import queue_startup_notice as _queue_startup_notice
 
     _sync_to_session()
     _queue_startup_notice(notice)
@@ -112,7 +112,7 @@ def queue_startup_markdown_notice(
     right_info: str | None = None,
     agent_name: str | None = None,
 ) -> None:
-    from fast_agent.ui.prompt.session import (
+    from fast_agent.ui.prompt.input import (
         queue_startup_markdown_notice as _queue_startup_markdown_notice,
     )
 
