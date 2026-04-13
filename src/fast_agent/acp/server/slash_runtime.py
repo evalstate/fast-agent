e6b05b408ab40cbb7152c3642a07d5ac9974952b
evalstate <1936278+evalstate@users.noreply.github.com>
2026-04-13 19:59:54 +0100

    structured cli option, connectors

--- src/fast_agent/acp/server/slash_runtime.py
+++ src/fast_agent/acp/server/slash_runtime.py
@@ -213,7 +213,7 @@ class ACPServerSlashRuntime:
             session_state.session_id,
             instance,
             self._host._resolve_primary_agent_name(instance) or "default",
-            noenv=instance.app.noenv_mode,
+            noenv=instance.app._noenv_mode,
             client_info=self._host._client_info,
             client_capabilities=self._host._client_capabilities,
             protocol_version=self._host._protocol_version,
